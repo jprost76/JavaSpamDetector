2693a1ae341300dfe492673bd4382fe09af21c35
LucasPc <LucasPc@DESKTOP-AHSSSIL>
2018-11-29 16:11:48 +0100

    Extraction Corpus Ok, lecture totale (No pretraitement)

--- ProjetSpam/src/Extracteur.java
+++ ProjetSpam/src/Extracteur.java
@@ -1,13 +1,20 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Extracteur {
 
-	TableOccur toccur;
+	TableOccur toccurSpam;
+	TableOccur toccurHam;
 	
-	public void extraire(String pfichier) {
+	Extracteur(){
+		this.toccurSpam= new TableOccur();
+		this.toccurHam= new TableOccur();
+	}
+	
+	public void extraire(String pfichier,TableOccur toccur) {
 	
 	BufferedReader IN=null;
 	try {
@@ -21,22 +28,35 @@ public class Extracteur {
 		while ((ligne = IN.readLine ())!= null){
 		     mot = ligne.split (" ");
 		     for (int i = 0; i < mot.length; i++)
-		         if (this.toccur.containsKey(mot[i])) 
+		     {
+		    	 System.out.println(mot[i]);
+		         if (toccur.containsKey(mot[i])) 
 		         	{
-		        	int occur= this.toccur.get(mot[i]).intValue();
-		        	this.toccur.put(mot[i],occur+1);
+		        	int occur=toccur.get(mot[i]).intValue();
+		        	toccur.put(mot[i],occur+1);
 		         	}
 		         else
 		         	{
-		        	 this.toccur.put(mot[i],1);
+		        	 toccur.put(mot[i],1);
 		         	}
+		     }
 		}
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
-this.toccur.setNbmail(this.toccur.getNbmail()+1);
+toccur.setNbmail(toccur.getNbmail()+1);
 }
 	
-	public void extraireCorpus() {}
+	public void extraireCorpus(File repertoire,TableOccur toccur) {
+	String [] listefichiers; 
+	listefichiers=repertoire.list(); 
+	for (int i=0;i<listefichiers.length;i++)
+	{
+		String temp = repertoire.getName() +"/"  + listefichiers[i];
+		this.extraire(temp,toccur);
+	}
+	}
+	
 	
 }
+
