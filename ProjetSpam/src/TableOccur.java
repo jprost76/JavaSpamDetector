79b4aeaf11b20df8b13413e655e4107697bc040f
jprost76 <jean.prost@insa-rouen.fr>
2018-12-29 18:37:18 +0100

    Ajout de la méthode AfficherTrier(), permettant d'afficher les jetons présents dans la table par ordre d'occurence décroissant

--- ProjetSpam/src/TableOccur.java
+++ ProjetSpam/src/TableOccur.java
@@ -1,5 +1,7 @@
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.TreeSet;
 
 
 
@@ -26,6 +28,24 @@ public class TableOccur extends HashMap<String,Integer> {
 		return this.nbMail;
 	}
 	
+	/**affiche les jetons dans la table et le nombre d'occurence associée, par ordre d'occurence décroissant **/
+	public void AfficherTrier(){
+		TreeSet<String> jetonsTries = new TreeSet<String>(new Comparateur(this));
+		jetonsTries.addAll(this.keySet());
+		Iterator<String> it = jetonsTries.descendingIterator();
+		while (it.hasNext()) {
+			String j = it.next();
+			int occ = this.obtenirOccurence(j);
+			try {
+				System.out.printf(String.format("%-30s : %-10d",j,occ));
+			}
+			catch (Exception ex) {
+				System.out.print(j+"    :     "+occ);
+			}
+			System.out.println();
+		}
+	}
+	
 	public void Afficher() {
 		Iterator<HashMap.Entry<String, Integer>> it = this.entrySet().iterator();
 		while (it.hasNext()) {
@@ -39,6 +59,24 @@ public class TableOccur extends HashMap<String,Integer> {
 				System.out.print(j+"    :     "+occ);
 			}
 			System.out.println();
+		}
+	}
+	
+	/** classe permettant de comparer 2 jetons selon leur nombre d'occurences contenu dans la table en attribut **/
+	class Comparateur implements Comparator<String> {
+		
+		private TableOccur table;
+		
+		public Comparateur(TableOccur uneTable){
+			super();
+			table = uneTable;
+		}
+		
+		/** retourne 1 si j1 a plus d'occurence j2, 0 sinon **/
+		public int compare(String j1, String j2) {
+			int occ1 = table.getOrDefault(j1, 0);
+			int occ2 = table.getOrDefault(j2, 0);
+			return (occ1>occ2 ? 1 : (occ1 == occ2 ? 0 : -1));
 		}
 	}
 }
